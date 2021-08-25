e46c24340a7a6f57c71dc3335c5f0fb86838cb82
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2021-08-25 17:30:26 +0100

    SIR-5067 investigate timestream alt timeline approach

--- cmd/postTimesteam.go
+++ cmd/postTimesteam.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
@@ -11,6 +14,20 @@ import (
 
 type Session struct {
 	AwsSession *session.Session
+}
+
+type SiriusEvent struct {
+	userID 				int
+	personID 			int				//Client ID or Deputy ID
+	eventType			string			//Insert Update
+	eventClass			string			//Opg\Core\Model\Event\Common\TaskCreated
+	sourceEntityID 		int				//144
+	sourceEntityClass 	string			//Opg\Core\Model\Entity\Task\Task
+}
+
+func newSiriusEvent(userID int, personID int, eventType string, eventClass string, sourceEntityID int, sourceEntityClass string) *SiriusEvent {
+	e := SiriusEvent{userID: userID, personID: personID, eventType: eventType, eventClass: eventClass, sourceEntityID: sourceEntityID, sourceEntityClass: sourceEntityClass}
+	return &e
 }
 
 func PostTimestream() {
@@ -25,15 +42,56 @@ func PostTimestream() {
 	creds := stscreds.NewCredentials(sess, RoleArn)
 	awsConfig := aws.Config{Credentials: creds, Region: aws.String("eu-west-1")}
 
+	se:= newSiriusEvent(100, 1,"INSERT", "Opg\\Core\\Model\\Event\\Common\\TaskCreated", 144, "Opg\\Core\\Model\\Entity\\Task\\Task")
+
 	svc := timestreamwrite.New(sess, &awsConfig)
+
+	now := time.Now()
+	currentTimeInSeconds := now.Unix()
 
 	records := &timestreamwrite.WriteRecordsInput{
 		DatabaseName: aws.String("audit-service-poc"),
 		TableName: aws.String("audit-service-poc"),
-		Records: &timestreamwrite.Record{
-
-		}
+		Records: []*timestreamwrite.Record{
+			&timestreamwrite.Record{
+				Dimensions: []*timestreamwrite.Dimension{
+					&timestreamwrite.Dimension{
+						Name:  aws.String("User ID"),
+						Value: aws.String(string(rune(se.userID))),
+					},
+					&timestreamwrite.Dimension{
+						Name:  aws.String("Person ID"),
+						Value: aws.String(string(rune(se.personID))),
+					},
+					&timestreamwrite.Dimension{
+						Name:  aws.String("Event Type"),
+						Value: aws.String(se.eventType),
+					},
+					&timestreamwrite.Dimension{
+						Name:  aws.String("Event Class"),
+						Value: aws.String(se.eventClass),
+					},
+					&timestreamwrite.Dimension{
+						Name:  aws.String("Source Entity ID"),
+						Value: aws.String(string(rune(se.sourceEntityID))),
+					},
+					&timestreamwrite.Dimension{
+						Name:  aws.String("Source Entity Class"),
+						Value: aws.String(se.sourceEntityClass),
+					},
+				},
+				Time:             aws.String(strconv.FormatInt(currentTimeInSeconds, 10)),
+				TimeUnit:         aws.String("SECONDS"),
+			},
+		},
 	}
 
-	svc.WriteRecords(records)
+	_, err = svc.WriteRecords(records)
+
+	if err != nil {
+		fmt.Println("Error:")
+		fmt.Println(err)
+	} else {
+		fmt.Println("Write records is successful")
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/tomtucka/audit-service-poc/cmd"
 )
 
 func main() {
@@ -10,10 +11,15 @@ func main() {
 		fmt.Println("Usage: audit-service -post-timestream")
 	}
 
-	postTimestream := flag.Bool("post-timetstream", false, "Post to timestream")
+	var postTimeStream bool
+
+	flag.BoolVar(&postTimeStream, "post-timestream", false, "Post to timestream")
 	flag.Parse()
 
-	if postTimestream {
+	if postTimeStream {
+		fmt.Println("Posting timestream")
 		cmd.PostTimestream()
+	} else {
+		fmt.Println("Unable to post to timestream")
 	}
 }
